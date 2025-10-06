922b9ac2afb045e8ef457976a50ba72559970902
I love kludge <98ger@mail.ru>
2025-10-06 14:21:14 +1200

    add switch ref on main page

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,24 @@
 
     <div class="main-box">
       <div class="navigation-panel">
+        <div class="section">
+          <div class="section-caption">
+            Inputs
+          </div>
 
+          <div class="section-options">
+            <div class="section-option" @click="currentComponent = 'Switch'">
+              Switch
+            </div>
+          </div>
+        </div>
       </div>
       <div class="component-view">
         <div class="component-example">
-
+          <component :is="currentComponent" v-model="testBoolValue"></component>
         </div>
         <div class="component-description">
-          
+
         </div>
       </div>
     </div>
@@ -25,11 +35,19 @@
 </template>
 
 <script>
+import Switch from './components/inputs/Switch.vue';
+
 export default {
   name: 'App',
-  data() { return {
-
-  }},
+  components: {
+    Switch,
+  },
+  data() {
+    return {
+      currentComponent: null,
+      testBoolValue: false,
+    }
+  },
 }
 
 </script>
@@ -57,6 +75,33 @@ header {
   border: solid var(--border-color);
   border-width: 0 1px 0 0;
   width: 300px;
+  padding: 16px;
+}
+
+.section {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.section-caption {
+  font-size: var(--big-font-size);
+}
+
+.section-options {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 0 0 0 16px;
+}
+
+.section-option {
+  user-select: none;
+  cursor: pointer;
+}
+
+.component-view {
+  padding: 64px;
 }
 
 footer {
