5f204a850c299680ffde25a4973911ba00e8256a
I love kludge <98ger@mail.ru>
2025-10-07 14:52:56 +1200

    add component view

---
+++ src/views/inputs/DropDownView.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="component-view">
+        <drop-down
+            v-model="testValue"
+            :options="testOptions"
+            :width="200"
+        />
+        <div class="description">
+            <h3>Drop-Down component</h3>
+            <div class="description">
+                <p>Custom drop down with specific params and different types</p>
+                <p><b>Props:</b></p>
+                <p>modelValue: </p>
+                <p>options: </p>
+                <p>type: </p>
+                <p>width: </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import DropDown from '@/components/inputs/drop-down/DropDown.vue';
+
+export default {
+    name: 'SwitchView',
+    components: {
+        DropDown
+    },
+    data() {
+        return {
+            testValue: false,
+            testOptions: [
+                'Dog',
+                'Cat',
+                'Frog',
+                'Whale'
+            ],
+        }
+    }
+};
+</script>
+
+<style scoped>
+.component-view {
+    display: flex;
+    flex-direction: row;
+    gap: 16px;
+}
+</style>
