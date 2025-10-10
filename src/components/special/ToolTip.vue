5facd196382355be04c107acb14a483175bb10a6
I love kludge <98ger@mail.ru>
2025-10-10 16:51:01 +1200

    init tool tip component :sparkles:

---
+++ src/components/special/ToolTip.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="tool-tipped-box">
+    <slot class="">
+
+    </slot>
+    <div class="tool-tip">
+      <slot name="text">
+        <div class="simple-description">
+          {{ description }}
+        </div>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToolTip',
+  props: {
+    description: {type: String, default: null},
+  },
+  data() {
+    return {
+      toolTipPosition: {
+        top: null,
+        left: null,
+      }
+    }
+  },
+  methods: {
+    mouseEnter() {},
+  }
+};
+</script>
+
+<style scoped>
+
+.tool-tipped-box {
+  position: relative;
+}
+
+.tool-tip {
+  position: absolute;
+}
+
+</style>
