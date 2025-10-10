882e91efe469ce427320e25a3677608a4b6baaf8
I love kludge <98ger@mail.ru>
2025-10-10 16:51:45 +1200

    add tool tip view

---
+++ src/views/special/ToolTipView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="component-view">
+    <tool-tip>
+      Some text with tool-tip
+      <template></template>
+    </tool-tip>
+
+    <div class="description">
+      <h3>Tool-Tip component</h3>
+      <div class="description">
+        <p></p>
+        <p><b>Props:</b></p>
+        <p></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ToolTip from '@/components/special/ToolTip.vue';
+
+export default {
+  name: 'ToolTipView',
+  components: {
+    ToolTip,
+  },
+};
+</script>
+
+<style scoped>
+.component-view {
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+}
+</style>
