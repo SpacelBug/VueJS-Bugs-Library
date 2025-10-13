a0a1465c51ddf43dfe9d3daa24de41beae4801f4
I love kludge <98ger@mail.ru>
2025-10-13 13:03:11 +1200

    show two various of component into component`s view

--- src/views/special/ToolTipView.vue
+++ src/views/special/ToolTipView.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="component-view">
-    <tool-tip>
-      Some text with tool-tip
-      <template></template>
-    </tool-tip>
+    <div class="component-variants">
+      <div class="component-variant">
+        <div class="caption"> Set tool-tip content by prop</div>
+        <tool-tip :description="'Some tip text'">
+          Text with tool-tip
+        </tool-tip>
+      </div>
+      <div class="component-variant">
+        <div class="caption"> Set tool-tip content by slot</div>
+        <tool-tip :description="'Some tip text'">
+          Text with tool-tip
+          <template v-slot:text>
+            <p><b>Some slotted text</b></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur tenetur totam nisi ullam repellendus
+              excepturi illo earum illum nam dolorum praesentium, nulla voluptatem ab ex alias quaerat optio sed
+              sapiente!
+            </p>
+          </template>
+        </tool-tip>
+      </div>
+    </div>
+
 
     <div class="description">
       <h3>Tool-Tip component</h3>
@@ -32,5 +50,17 @@ export default {
   display: flex;
   flex-direction: row;
   gap: 16px;
+}
+
+.component-variants {
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+}
+
+.component-variant {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 }
 </style>
