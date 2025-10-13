5d4e10805d8f945bb58e9390be506125ca1f910e
I love kludge <98ger@mail.ru>
2025-10-13 13:02:34 +1200

    add ToolTip link into SpecialComponents view

--- src/views/sections/SpecialSection.vue
+++ src/views/sections/SpecialSection.vue
@@ -1,29 +1,34 @@
 <template>
-    <div class="components">
-
-    </div>
+  <div class="components">
+    <router-link
+        :to="{name: 'tool-tip'}"
+        class="side-link"
+    >
+      Tool-Tip
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'SpecialSection',
+  name: 'SpecialSection',
 };
 </script>
 
 <style scoped>
 .components {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    padding-left: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding-left: 16px;
 }
 
 .side-link {
-    color: white;
-    text-decoration: none;
+  color: white;
+  text-decoration: none;
 }
 
 .router-link-active {
-    color: var(--accent-color)
+  color: var(--accent-color)
 }
 </style>
