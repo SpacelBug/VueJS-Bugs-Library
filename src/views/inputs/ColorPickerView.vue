5ff439ed436a7815f06899ad6ad0b9d19b5221b9
I love kludge <98ger@mail.ru>
2025-10-21 16:03:48 +1200

    create view for ColorPicker

---
+++ src/views/inputs/ColorPickerView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="component-view">
+    <color-picker></color-picker>
+
+    <div class="description">
+      <h3>ColorPicker component</h3>
+      <div class="description">
+        <p></p>
+        <p><b>Props:</b></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ColorPicker from '@/components/inputs/color-picker/ColorPicker.vue';
+
+export default {
+  name: 'ColorPickerView',
+  components: {
+    ColorPicker,
+  },
+  data() {
+    return {
+    }
+  }
+};
+</script>
+
+<style scoped>
+.component-view {
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+}
+</style>
