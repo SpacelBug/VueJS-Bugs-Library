c67e8ae1264d29eee8c3c9d591184cef381498e7
I love kludge <98ger@mail.ru>
2025-10-21 16:03:29 +1200

    init new component ColorPicker :sparkles:

---
+++ src/components/inputs/color-picker/ColorPicker.vue
@@ -0,0 +1,293 @@
+<template>
+  <div
+      class="main-box"
+      @focusin="isShowPicker = true"
+      @focusout="isShowPicker = false"
+      tabindex="0"
+  >
+    <div class="picked-color">
+      <div class="color" />
+    </div>
+    <div
+        v-show="isShowPicker"
+        @mousedown="isMouseDown = true"
+        @mouseup="isMouseDown = false"
+        class="picker"
+    >
+      <div class="palette-box">
+        <canvas
+            @mousemove="paletteMove"
+            :width="circleSize"
+            :height="circleSize"
+            ref="rainbow"
+        />
+        <div class="caret" v-if="paletteCaretPos.length > 0"/>
+      </div>
+      <div class="additional-params">
+        <div class="param">
+          Light
+          <div class="param-gradient">
+            <canvas
+                v-show="color"
+                class="light-gradient"
+                :width="circleSize"
+                height="30"
+                ref="light"
+                @mousemove="lightMove"
+            />
+            <div class="light-caret" />
+          </div>
+        </div>
+        <div class="param">
+          Transparency
+          <div class="param-gradient">
+            <canvas
+                v-show="color"
+                class="transparent-gradient"
+                :width="circleSize"
+                height="30"
+                ref="transparent"
+                @mousemove="transparencyMove"
+            />
+            <div class="transparency-caret" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorPicker',
+  props: {
+    modelValue: { type: String, default: '#FFFFFF' },
+    circleSize: { type: Number, default: 300 }
+  },
+  data() {
+    return {
+      color: null,
+
+      isShowPicker: false,
+      isMouseDown: false,
+
+      paletteCaretPos: [],
+      lightCaretPos: 0,
+      transparencyCaretPos: 0,
+    }
+  },
+  mounted() {
+    this.drawRainbowGradient()
+  },
+  watch: {
+    color() {
+      this.drawLightGradient()
+      this.drawTransparentGradient()
+    }
+  },
+  computed: {
+    resultColor() {
+      return this.color
+    }
+  },
+  methods: {
+    drawRainbowGradient() {
+      const canvas = this.$refs.rainbow
+      const ctx = canvas.getContext('2d');
+      const cx = canvas.width / 2;
+      const cy = canvas.height / 2;
+      const radius = Math.min(cx, cy);
+
+      // count of steps for smooth gradient
+      const steps = 1000;
+      for (let i = 0; i < steps; i++) {
+        const start = (i * Math.PI) / 180;
+        const end = ((i + 1) * Math.PI) / 180;
+
+        ctx.beginPath();
+        ctx.moveTo(cx, cy);
+        ctx.arc(cx, cy, radius, start, end);
+        ctx.closePath();
+
+        const hue = i; // smooth transition of colors
+        ctx.fillStyle = `hsl(${hue}, 100%, 50%)`;
+        ctx.fill();
+      }
+    },
+    drawLightGradient() {
+      const canvas = this.$refs.light
+      const ctx = canvas.getContext('2d');
+      const gradient = ctx.createLinearGradient(0, canvas.height, canvas.width, canvas.height);
+
+      gradient.addColorStop(0, this.color);
+      gradient.addColorStop(1, 'rgba(0, 0, 0, 1)');
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.fillStyle = gradient;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    },
+    drawTransparentGradient() {
+      const canvas = this.$refs.transparent
+      const ctx = canvas.getContext('2d');
+      const gradient = ctx.createLinearGradient(0, canvas.height, canvas.width, canvas.height);
+
+      gradient.addColorStop(0, this.color);
+      gradient.addColorStop(1, this.color.replace('rgb', 'rgba').replace(')', ', 0)'))
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.fillStyle = gradient;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    },
+    paletteMove() {
+      if (!this.isMouseDown) return;
+      const rect = this.$refs.rainbow.getBoundingClientRect();
+      const ctx = this.$refs.rainbow.getContext('2d')
+
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      const pixel = ctx.getImageData(x, y, 1, 1).data;
+      const color = `rgb(${pixel[0]}, ${pixel[1]}, ${pixel[2]})`;
+
+      this.paletteCaretPos = [x, y]
+      this.color = color
+    },
+    lightMove() {
+      if (!this.isMouseDown) return
+      const rect = this.$refs.light.getBoundingClientRect()
+      const x = event.clientX - rect.left
+      this.lightCaretPos = x
+    },
+    transparencyMove() {
+      if (!this.isMouseDown) return
+      const rect = this.$refs.transparent.getBoundingClientRect()
+      const x = event.clientX - rect.left
+      this.transparencyCaretPos = x
+    },
+  }
+};
+</script>
+
+<style scoped>
+.main-box {
+  position: relative;
+}
+
+.picked-color {
+  box-sizing: border-box;
+  background-color: var(--panels-color);
+  border-radius: 5px;
+  width: 35px;
+  height: 35px;
+  padding: 5px;
+}
+
+.color {
+  background-color: v-bind(color);
+  height: 100%;
+  width: 100%;
+  border-radius: 5px;
+}
+
+.picker {
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  padding: 16px;
+  gap: 16px;
+  border-radius: 5px;
+  background-color: var(--panels-color);
+}
+
+.palette-box {
+  position: relative;
+}
+
+.palette-box canvas {
+  border-radius: 50%;
+}
+
+.caret {
+  pointer-events: none;
+  position: absolute;
+  background-color: white;
+  border: solid 1px black;
+  border-radius: 50%;
+  top: v-bind(paletteCaretPos[1] - 3 + 'px');
+  left: v-bind(paletteCaretPos[0] - 3 + 'px');
+  width: 6px;
+  height: 6px;
+}
+
+.additional-params {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.additional-params .param-gradient {
+  position: relative;
+}
+
+.light-caret, .transparency-caret {
+  cursor: pointer;
+  position: absolute;
+  border: solid 1px black;
+  background-color: white;
+  top: 0;
+  border-radius: 5px;
+  height: 30px;
+  width: 5px;
+}
+
+.light-caret {
+  left: v-bind(lightCaretPos + 'px');
+}
+
+.transparency-caret {
+  left: v-bind(transparencyCaretPos + 'px');
+}
+
+.param {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+}
+
+.light-gradient {
+  border: solid 1px black;
+  cursor: pointer;
+}
+
+.transparent-gradient {
+  border: solid 1px black;
+  border-radius: 5px;
+  cursor: pointer;
+  background-size: contain;
+}
+
+.color-plates {
+  display: grid;
+}
+
+.plate {
+  width: 30px;
+  height: 30px;
+}
+
+.color-gradient {
+  width: 200px;
+  height: 200px;
+  border-radius: 5px;
+  margin-top: 10px;
+  cursor: pointer;
+  background: linear-gradient(90deg, white, red);
+}
+
+.light {
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  mix-blend-mode: multiply;
+  background: linear-gradient(to bottom, transparent, black);
+}
+</style>
