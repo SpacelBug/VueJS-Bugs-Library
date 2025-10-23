2e35db62241f595333971f71d772df62d05b254f
I love kludge <98ger@mail.ru>
2025-10-23 12:58:15 +1200

    create file input view

---
+++ src/views/inputs/FileInputView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="component-view">
+    <FileInput v-model="file" />
+    <div class="description">
+      <h3>File input component</h3>
+      <div class="description">
+        <p></p>
+        <p><b>Props:</b></p>
+        <p>modelValue: </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FileInput from '@/components/inputs/file/FileInput.vue';
+
+export default {
+  name: 'fileInputView',
+  components: {
+    FileInput
+  },
+  data() {
+    return {
+      file: null
+    }
+  }
+};
+</script>
+
+<style scoped>
+.component-view {
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+}
+</style>
