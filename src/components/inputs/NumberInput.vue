bce22a845ac08c8c1edc75b9ecb5a2175a29d0f0
I love kludge <98ger@mail.ru>
2025-12-05 16:01:08 +1200

    Init new component NumberInput :sparkles:

---
+++ src/components/inputs/NumberInput.vue
@@ -0,0 +1,54 @@
+<template>
+  <input
+      ref="input"
+      :value="modelValue"
+      @keydown="onKeyDown"
+      @change="onChange"
+      @input="onInput"
+  >
+</template>
+
+<script>
+export default {
+  name: "NumberInput",
+  emits: ["update:modelValue", "change"],
+  props: {
+    modelValue: { type: Number, required: true },
+    fitContent: { type: Boolean, default: false },
+  },
+  computed: {
+    fieldSizing() {
+      return this.fitContent ? 'content' : 'unset'
+    }
+  },
+  methods: {
+    onKeyDown() {
+      const reg = /[0-9,.]/
+      const excludedKeyCodes = ['Backspace', 'Enter', 'Delete', 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown']
+      if (!reg.test(event.key) && !excludedKeyCodes.includes(event.key)) {
+        event.preventDefault()
+      }
+    },
+    onChange() {
+      this.$emit('update:modelValue', Number(event.target.value))
+    },
+    onInput() {
+      this.$emit('change', event)
+    }
+  }
+}
+</script>
+
+<style scoped>
+input {
+  field-sizing: v-bind(fieldSizing);
+  box-sizing: border-box;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  padding: 8px 16px;
+  height: fit-content;
+  background-color: var(--panels-color);
+  color: var(--font-color);
+}
+</style>
