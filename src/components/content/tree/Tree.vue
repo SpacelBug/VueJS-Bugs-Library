fbe562a6c907b303f55f19d4e8ae44b73837b015
I love kludge <98ger@mail.ru>
2025-12-17 14:31:17 +1200

    add new component Tree :sparkles:

---
+++ src/components/content/tree/Tree.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tree-box">
+    <TreeNode
+        v-for="node in nodes"
+        class="node"
+        :node="node"
+        :checkable="checkable"
+    />
+  </div>
+</template>
+
+<script>
+import TreeNode from './TreeNode.vue';
+
+/**
+ * Vue js tree component
+ * @nodes
+ */
+export default {
+  name: "Tree",
+  emits: ['nodeClick'],
+  components: {
+    TreeNode,
+  },
+  props: {
+    /**
+     * Array of Objects
+     * ```js
+     * [
+     *  {name: 'node name'}, 
+     *  {name: 'node name'}, 
+     *  {name: 'node name', nodes: [
+     *    {name: 'nested node name'}, 
+     *    {name: 'nested node name'}, 
+     *    {name: 'nested node name'}
+     *  ]}
+     * ]
+     * ```
+     */
+    nodes: { type: [Object], required: true },
+    /**
+     * Adding checkbox for every node
+     * @values true, false
+     */
+    checkable: { type: Boolean, default: false },
+  },
+  data() {
+    return {
+      checkedNodes: [],
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tree-box {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+}
+</style>
---
+++ src/components/content/tree/TreeNode.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="node">
+    <div class="icon-box">
+      <div
+          v-if="node.nodes"
+          :class="['icon', {'opened': isShowNested}]"
+          @click="isShowNested = !isShowNested"
+      />
+    </div>
+
+    <input v-if="checkable" type="checkbox">
+
+    <div class="node-name">
+      {{ node.name }}
+    </div>
+
+    <div
+        v-if="isShowNested && node.nodes"
+        class="nested-nodes"
+    >
+      <TreeNode
+          v-for="node in node.nodes"
+          :node="node"
+          :checkable="checkable"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * Tree node component nodes
+ */
+export default {
+  name: "TreeNode",
+  props: {
+    node: { required: true },
+    checkable: { type: Boolean },
+  },
+  data() {
+    return {
+      isShowNested: false,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon-box {
+  width: 15px;
+  aspect-ratio: 1/1;
+  grid-area: icon;
+}
+
+.icon {
+  mask-image: url("@/assets/icons/SmallArrowHead.svg");
+  mask-size: contain;
+  cursor: pointer;
+  background-color: var(--font-color);
+  width: 100%;
+  height: 100%;
+  rotate: -90deg;
+}
+
+.opened {
+  rotate: 0deg;
+}
+
+input {
+  grid-area: checkbox;
+  height: 15px;
+  width: 15px;
+}
+
+.node {
+  display: grid;
+  align-items: center;
+  grid-template: "icon checkbox name"
+    "nested nested nested" auto / 15px min-content auto;
+  grid-gap: 4px;
+}
+
+.node-name {
+  grid-area: name;
+}
+
+.nested-nodes {
+  display: flex;
+  flex-direction: column;
+  margin-left: 16px;
+  gap: 4px;
+  grid-area: nested;
+}
+</style>
