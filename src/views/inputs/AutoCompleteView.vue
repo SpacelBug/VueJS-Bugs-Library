14f4d06ea94283c8c1fc73d4fd401ca6a09b1462
I love kludge <98ger@mail.ru>
2026-01-19 15:30:27 +1200

    add view for new component AutoComplete

---
+++ src/views/inputs/AutoCompleteView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="component-view">
+    <div class="component-variants">
+      <auto-complete :completeOptions="completeOptions"></auto-complete>
+    </div>
+    <div class="description"></div>
+  </div>
+</template>
+<script>
+import AutoComplete from '@/components/inputs/AutoComplete.vue';
+
+export default {
+  name: "CheckBoxView",
+  components: {
+    AutoComplete,
+  },
+  data() {
+    return {
+      completeOptions: [
+        'some text with some information', 
+        'and some another text with different information',
+        'some extra super different text'
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.component-view {
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+}
+
+.component-variants {
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+}
+
+.component-variant {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+</style>
