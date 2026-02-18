b55e88edd9ab27fa7616e1b8b7f6cc1778ab0f45
I love kludge <98ger@mail.ru>
2026-02-18 17:07:29 +1200

    scoped styles

--- src/components/special/Tags.vue
+++ src/components/special/Tags.vue
@@ -81,7 +81,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .tags {
   position: relative;
   display: flex;
--- src/views/special/TagsView.vue
+++ src/views/special/TagsView.vue
@@ -48,7 +48,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .component-view {
   display: flex;
   flex-direction: row;
