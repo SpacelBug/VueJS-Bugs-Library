400ab8199de929ae022c8f6e56ca014c5debee39
I love kludge <98ger@mail.ru>
2026-03-31 11:35:33 +1200

    remove some code and add styles

--- src/views/webgl/WebGLView.vue
+++ src/views/webgl/WebGLView.vue
@@ -1,7 +1,17 @@
 <template>
   <h3>WebGL</h3>
 
-  <canvas ref="canvas" width="1000" height="600"/>
+  <div class="work-area">
+    <canvas
+        ref="canvas"
+        width="1000"
+        height="600"
+    />
+
+    <div class="params">
+
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -11,41 +21,32 @@ import { onMounted, ref, render } from "vue"
 const canvas = ref(null)
 
 onMounted(() => {
-  const gl = canvas.value.getContext("webgl")
-  /** Render 3d objects (models) in webgl
-   * real 3D objects
-  */
-  function render(modelPath) {
-    // Load the model from the specified path
-    let path
+  const gl = canvas.value.getContext("webgl2")
+  
+  if (!gl) console.error("HAVE NO WebGL2 context")
 
-    // Set up WebGL context and shaders
-    // const shaderProgram = initShaders(gl)
-
-    // Render loop
-    function renderLoop() {
-      // Clear the canvas
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-      // Draw the model using the shader program
-      // drawModel(gl, shaderProgram, model)
-
-      // Request the next frame
-      requestAnimationFrame(renderLoop)
-    }
-
-    // Start the render loop
-    renderLoop()
-  }
-
-  render('./sphere.obj')
+  
 })  
 
 </script>
 
 <style scoped>
+
+.work-area {
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+}
+
 canvas {
   border: solid var(--border-color) 2px;
   border-radius: 5px;
 }
+
+.params {
+  background-color: var(--panels-color);
+  width: 100%;
+  border-radius: 5px;
+}
+
 </style>
