791a6d36184b32ed1886f50cc03d23dac570a1aa
I love kludge <98ger@mail.ru>
2026-04-30 16:04:18 +1200

    improve description

--- src/views/content/SpoilerView.vue
+++ src/views/content/SpoilerView.vue
@@ -1,19 +1,38 @@
 <template>
     <div class="component-view">
-        <Spoiler>
-            <template v-slot:caption>
-                Spoiler text
-            </template>
-            <template v-slot:content>
-                <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam excepturi doloribus vitae vel ipsa iure voluptates natus debitis quidem, corrupti odit, adipisci magni, esse et enim numquam dolorem? Sunt, amet.</div>
-            </template>
-        </Spoiler>
+        <div class="component-variants">
+            <div class="component-variant">
+                <Spoiler>
+                    <template v-slot:caption>
+                        Spoiler text
+                    </template>
+                    <template v-slot:content>
+                        <h3>Some header</h3>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, fugiat eos autem
+                        distinctio, ipsam corrupti vero nesciunt, ipsum sequi laudantium minus. Ipsum iure animi libero
+                        excepturi, quisquam inventore repellendus sit. Lorem ipsum dolor sit, amet consectetur
+                        adipisicing elit. Sit consectetur expedita numquam, illo tempora fuga aliquam quod neque fugiat
+                        natus error, quis, necessitatibus assumenda amet labore dolorum sequi cumque similique.
+                    </template>
+                </Spoiler>
+                <h3>Other content</h3>
+                <div class="other-content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consequatur totam quos architecto,
+                    quisquam voluptatum sed cupiditate libero aperiam magnam possimus alias earum reprehenderit ea
+                    molestias voluptate similique? Accusantium, aliquid.
+                </div>
+            </div>
+        </div>
         <div class="description">
             <h3>Spoiler component</h3>
+            Component for hide some information. Realized by slots <br>
             <div class="description">
                 <p></p>
+                <p><b>Slots:</b></p>
+                <p><u>caption</u> - visible part</p>
+                <p><u>content</u> - hidden content</p>
                 <p><b>Props:</b></p>
-                <p>width:</p>
+                <p><u>width:</u> - width of hidden content in px. Default is 100%</p>
             </div>
         </div>
     </div>
@@ -39,6 +58,14 @@ export default {
 .component-view {
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
     gap: 16px;
+}
+
+.component-variant {
+    display: flex;
+    flex-direction: column;
+    width: 500px;
+    gap: 8px;
 }
 </style>
